16b41976828c4fdc6f5f26f4a532b4598ca16661
NemesisTCO <mfa200312@gmail.com>
2024-04-15 17:51:55 -0500

    Added leetcode1672.py

--- Python/leetcode1672.py
+++ Python/leetcode1672.py
@@ -2,7 +2,6 @@
 LEETCODE 1672. Richest Customer Wealth
 
 You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
-
 A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
 
 Example 1:
@@ -27,7 +26,6 @@ Input: accounts = [[2,8,7],[7,1,3],[1,9,5]]
 Output: 17
 
 Constraints:
-
 m == accounts.length
 n == accounts[i].length
 1 <= m, n <= 50
