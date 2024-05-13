b9d1c6037ec6b7f3273e9a3ca0b5ca63104d84c0
NemesisTCO <mfa200312@gmail.com>
2024-05-13 14:03:06 -0500

    Added leetcode20.py

---
+++ Python/leetcode20.py
@@ -0,0 +1,47 @@
+'''
+LEETCODE 20. Valid Parentheses
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+ 
+Constraints:
+- 1 <= s.length <= 104
+- s consists of parentheses only '()[]{}'.
+'''
+
+def isValid(s):
+    stk = []
+    pairs = {')': '(', ']': '[', '}': '{'}
+
+    for char in s:
+        if char in pairs.values():
+            stk.append(char)
+        elif not stk or stk.pop() != pairs[char]:
+            return False
+    
+    return not stk
+
+def main():
+    s = "(]"
+    print(isValid(s))
+
+if __name__ == "__main__":
+    main()
